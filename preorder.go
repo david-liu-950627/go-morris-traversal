141c7962e843aa02c7d3e20f12089043ddd7b4d5
David Liu <msglight4874@gmail.com>
2021-07-25 18:08:47 +0800

    add preorder

---
+++ preorder.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+	current := root
+	result := []int{}
+
+	for current != nil {
+		if current.Left == nil {
+			result = append(result, current.Val)
+			current = current.Right
+		} else {
+			predecessor := current.Left
+			for predecessor.Right != nil && predecessor.Right != current {
+				predecessor = predecessor.Right
+			}
+
+			if predecessor.Right == nil {
+				result = append(result, current.Val)
+				predecessor.Right = current
+				current = current.Left
+			} else {
+				predecessor.Right = nil
+				current = current.Right
+			}
+		}
+	}
+
+	return result
+}
