08097d55d48c3776b14dc99eea0fbdc5a5055d09
David Liu <msglight4874@gmail.com>
2021-07-25 18:08:10 +0800

    add inorder

---
+++ inorder.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+  result := []int{}
+  
+  current := root
+  for current != nil {
+    if current.Left == nil {
+      result = append(result, current.Val)
+      current = current.Right
+    } else {
+      predecessor := current.Left
+      for predecessor.Right != nil && predecessor.Right != current {
+        predecessor = predecessor.Right
+      }
+      
+      if predecessor.Right == nil {
+        predecessor.Right = current
+        current = current.Left
+      } else {
+        predecessor.Right = nil
+        result = append(result, current.Val)
+        current = current.Right
+      }
+    }
+  }
+  
+  return result
+}
